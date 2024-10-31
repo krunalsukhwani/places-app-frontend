b0982a25a362a29230b6f65ff5796dd547c3f484
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-10-31 19:58:22 -0400

    Users and Shared Components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 import "./Users.css";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>User Component!!</h2>;
+    const USERS = [
+        {
+            "id" : "user1",
+            "name" : "Steven",
+            "places" : 4,
+            "image" : "https://i.imgur.com/OB0y6MR.jpg"
+        }
+    ]
+  return <UsersList items={USERS}/>;
 };
 
 export default Users;
