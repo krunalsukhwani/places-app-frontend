47f005469981fcda2890adf9b2a1be58d5734d99
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-11-07 19:51:15 -0500

    Added Side Drawer

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -10,8 +10,14 @@ const NavLinks = (props) => {
         <NavLink to="/" exact>
           ALL USERS
         </NavLink>
+      </li>
+      <li>
         <NavLink to="/u1/places">MY PLACES</NavLink>
+      </li>
+      <li>
         <NavLink to="/places/new">ADD PLACE</NavLink>
+      </li>
+      <li>
         <NavLink to="/auth">AUTHENTICATE</NavLink>
       </li>
     </ul>
