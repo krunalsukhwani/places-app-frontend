d6750b57843403de1e59dce190cd706a450fdef0
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-11-07 19:08:27 -0500

    Card Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -6,7 +6,7 @@ import UsersList from "../components/UsersList";
 const Users = () => {
     const USERS = [
         {
-            "id" : "user1",
+            "id" : "u1",
             "name" : "Steven",
             "places" : 4,
             "image" : "https://i.imgur.com/OB0y6MR.jpg"
